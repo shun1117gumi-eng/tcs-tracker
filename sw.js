06715ce06ef8e013ed02a2da3601d4c6f233d87f
shun1117gumi-eng <shun.1117gumi@gmail.com>
2026-03-29 10:36:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,10 @@
-const CACHE = 'tcs-tracker-v3';
-const ASSETS = ['/tcs-tracker/', '/tcs-tracker/index.html', '/tcs-tracker/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
-  self.skipWaiting();
-});
-
+self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).then(res => {
-      if (res && res.status === 200) {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
